984d361da00e2a24da721f45693cd24c7fcdd374
Alen Budimir <7889389+budi1200@users.noreply.github.com>
2021-09-20 23:09:57 +0200

    Add message helper

--- src/main/kotlin/si/budimir/dataMigrator/commands/subcommands/ReloadSubCommand.kt
+++ src/main/kotlin/si/budimir/dataMigrator/commands/subcommands/ReloadSubCommand.kt
@@ -1,21 +1,22 @@
 package si.budimir.dataMigrator.commands.subcommands
 
-import net.kyori.adventure.text.minimessage.MiniMessage
 import org.bukkit.command.Command
 import org.bukkit.command.CommandSender
+import org.bukkit.entity.Player
 import si.budimir.dataMigrator.DataMigrator
 import si.budimir.dataMigrator.commands.SubCommandBase
+import si.budimir.dataMigrator.util.MessageHelper
 
 class ReloadSubCommand: SubCommandBase {
     override fun execute(sender: CommandSender, command: Command, label: String, args: Array<String>): Boolean {
         val config = DataMigrator.instance.getMainConfig()
         config.reloadConfig()
-        sender.sendMessage(MiniMessage.get().parse("<green>Reload Complete!"))
+        MessageHelper.sendMessage(sender as Player, "<green>Reload Complete!")
         return true
     }
 
     override fun getPermission(): String {
-        return "bcapture.admin"
+        return "datamigrator.admin"
     }
 
     override fun getDesc(): String {
---
+++ src/main/kotlin/si/budimir/dataMigrator/util/MessageHelper.kt
@@ -0,0 +1,42 @@
+package si.budimir.dataMigrator.util
+
+import net.kyori.adventure.text.Component
+import net.kyori.adventure.text.event.ClickEvent
+import net.kyori.adventure.text.event.HoverEvent
+import net.kyori.adventure.text.minimessage.MiniMessage
+import org.bukkit.entity.Player
+import si.budimir.dataMigrator.DataMigrator
+
+abstract class MessageHelper {
+    companion object {
+        private val plugin: DataMigrator = DataMigrator.instance
+        private val config = plugin.getMainConfig()
+        private val pluginPrefix = config.getParsedString("pluginPrefix")
+
+        // Send message with string from config
+        fun sendMessage(player: Player, key: String, placeholders: MutableMap<String, String>, prefix: Boolean = true) {
+            var tmp = Component.text("")
+
+            if (prefix) {
+                tmp = tmp.append(pluginPrefix)
+            }
+
+            tmp = tmp.append(config.getParsedString(key, placeholders))
+
+            player.sendMessage(tmp)
+        }
+
+        // Send message with provided string
+        fun sendMessage(player: Player, message: String, prefix: Boolean = true) {
+            var tmp = Component.text("")
+
+            if (prefix) {
+                tmp = tmp.append(pluginPrefix)
+            }
+
+            tmp = tmp.append(MiniMessage.markdown().parse(message))
+
+            player.sendMessage(tmp)
+        }
+    }
+}
